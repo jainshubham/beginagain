8c9eeae532c6bbd013f0cc2bc7f8195439cfce5a
Kanchan Srivastava <Kanchan.Srivastava@cdk.com>
2018-01-24 16:28:48 +0530

    fixed profile page right sidebar

--- profiles/views.py
+++ profiles/views.py
@@ -35,9 +35,17 @@ class MyProfile(ListView):
     context_object_name = 'profile'
     template_name = 'profile-my.html'
 
-    def get_queryset(self):
-        id = self.request.__dict__['user'].id
-        return get_object_or_404(Profile, id=id)
+    # def get_queryset(self):
+    #     id = self.request.__dict__['user'].id
+    #     return get_object_or_404(Profile, id=id)
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['profile'] = get_object_or_404(Profile, id=self.request.user.id)
+        context['profiles'] = Profile.objects.all()
+        return context
 
     def post(self, request, *args, **kwargs):
         """Not sure why post is redirecting here
