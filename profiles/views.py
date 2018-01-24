54063032f95f52808d7e7083362091b8acd626de
Kanchan Srivastava <Kanchan.Srivastava@cdk.com>
2018-01-24 15:21:42 +0530

    loginview post function fixed; now redirecting to profile list page

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,11 @@
 # Create your views here.
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate
 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.views.generic import ListView
+
 from profiles.models import Profile
 
 
@@ -48,15 +51,14 @@ class MyProfile(ListView):
         for key in Profile._meta.get_fields():
             value = request.POST.get(key.name)
             if value:
-                print(value)
                 setattr(instance, key.name, value)
         instance.save()
         return HttpResponseRedirect('')
 
+
 class LoginView(ListView):
     model = Profile
     context_object_name = 'profiles'
-
 
     def post(self, request, *args, **kwargs):
         """Not sure why post is redirecting here
@@ -66,9 +68,17 @@ class LoginView(ListView):
         :param kwargs:
         :return:
         """
-        # id = self.request.__dict__['user'].id
-        # instance = Profile.objects.get(id=id)
-        return HttpResponseRedirect('')  # render(request, 'profiles-listing.html')
+        email = request.POST.get('email')
+        password = request.POST.get('psw')
+        if not (email and password):
+            raise Exception("provide valid email and password.")
+        user = authenticate(email=email, password=password)
+        if not user:
+            raise Exception("provide valid email and password.")
+        profile = Profile.objects.get(email=email)
+        return render(request, 'profiles-listing.html',  {
+            'profiles': [profile]
+        },)
 
 
 class ShowInterest(ListView):
