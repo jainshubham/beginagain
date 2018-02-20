35ecbf3cc015e36bb7a672baaab11cc04cd827b7
Shubham Jain <shubham2006@gmail.com>
2018-02-20 09:14:02 +0000

    added city lights extension

--- beginagain/settings.py
+++ beginagain/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles',
-    'django_extensions'
+    'django_extensions',
+    'cities_light'
 ]
 
 MIDDLEWARE = [
